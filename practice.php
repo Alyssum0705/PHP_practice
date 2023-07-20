28ac842408415b9a6a7447ce48ae2a85d5ae3294
Arisa <bluelotus.melody@gmail.com>
2023-07-20 12:43:09 +0000

    PHP練習ファイル作成

---
+++ practice.php
@@ -0,0 +1,22 @@
+<?php
+$a = 3;
+$b = 7;
+echo $a + $b;
+
+echo "\n";
+
+$array = ["1月", "2月", "3月", "4月", "5月", "6月", "7月", "8月", "9月", "10月", "11月", "12月"];
+echo $array[7];
+
+echo "\n";
+
+$hello = "Hello, ";
+$name = "Arisa Kitazumi";
+$world = "'s World!";
+echo $hello . $name . $world;
+
+echo "\n";
+
+$tech_boost = "tech";
+$tech_boost .= " boost";
+echo $tech_boost;
